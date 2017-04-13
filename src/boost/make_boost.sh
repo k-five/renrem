4597d5b946cfb005a4739c4886d35e3e50be8395
Shekib <vhsu30@yahoo.com>
2017-04-13 14:59:10 +0430

    fix the 'cd' command

--- src/boost/make_boost.sh
+++ src/boost/make_boost.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-cp ../src/std/renrem_std.cpp renrem_boost.cpp
+cp ../std/renrem_std.cpp renrem_boost.cpp
 perl -i -lpe 's/regex(?=>)/regex.hpp/; s/std::(?=(regex|regex_search|regex_replace|smatch))/boost::/g' renrem_boost.cpp
